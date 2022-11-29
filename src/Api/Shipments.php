0f6cd01fe9070403f4fba94df601ee5ad62e746b
{ipking} <667997@gmail.com>
2022-11-29 17:05:09 +0800

    add code

--- src/Api/Shipments.php
+++ src/Api/Shipments.php
@@ -17,4 +17,18 @@ class Shipments extends Client {
 		  'json'   => $body,
 		]);
 	}
+	
+	/**
+	 * Operation correctShipments
+	 * @param string $carrier
+	 * @param string $trackingNumber
+	 * @param array $body
+	 */
+	public function correctShipments($carrier,$trackingNumber,$body = [])
+	{
+		return $this->send("/v1/shipments/carriers/'.$carrier.'/trackingnumbers/'.$trackingNumber.'/positionitems", [
+			'method' => 'POST',
+			'json'   => $body,
+		]);
+	}
 }
